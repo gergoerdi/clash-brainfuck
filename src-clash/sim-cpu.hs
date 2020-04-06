68a9149eb9e5ce89b12c473ec8882b8d19337bae
Dr. ERDI Gergo <gergo@erdi.hu>
2020-04-06 21:59:02 +0800

    Use MonadBFMemory in simulation

--- src-clash/sim-cpu.hs
+++ src-clash/sim-cpu.hs
@@ -1,31 +1,35 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Clash.Prelude
-import Brainfuck.CPU
+import Clash.Prelude hiding (lift)
+import Brainfuck.CPU hiding (output)
 import Brainfuck.Types
+import Brainfuck.IO
+import Brainfuck.Memory
 
-import Data.Array.IO
 import qualified Data.ByteString as BS
-import Data.List as L
-import Data.Char
 import System.IO
-import Control.Monad
 import Data.Foldable (for_)
 import Control.Monad.State.Strict
 import Control.Lens
-import Data.Functor.Identity
-import Data.Word
 
-newROM :: forall addr val. (Integral addr, Bounded addr) => [val] -> IO (addr -> IO val)
-newROM contents = do
-    arr <- newListArray @IOArray ((0 :: Int), fromIntegral (maxBound :: addr)) contents
-    return $ readArray arr . fromIntegral
 
-newRAM :: forall addr val. (Integral addr, Bounded addr, Num val) => IO (addr -> IO val, addr -> val -> IO ())
-newRAM = do
-    arr <- newArray @IOArray ((0 :: Int), fromIntegral (maxBound :: addr)) 0
-    return (readArray arr . fromIntegral, writeArray arr . fromIntegral)
+simulateCPU :: (MonadBFMemory m, MonadBFIO m) => StateT (CPUState, Raw CPUOut) m ()
+simulateCPU = do
+    (s, out@CPUOut{..}) <- get
+
+    instr <- lift $ readProgROM _progAddr
+    memRead <- lift $ readRAM _memAddr
+    let input = CPUIn
+            { outputAck = True
+            , input = Nothing
+            , ..
+            }
+    out'@CPUOut{..} <- zoom _1 $ mapStateT (return . runIdentity) $ step' input
+    zoom _2 $ put out'
+
+    for_ _output $ lift . output
+    for_ _memWrite $ lift . writeRAM _memAddr
 
 main :: IO ()
 main = do
@@ -33,30 +37,6 @@ main = do
 
     prog <- BS.readFile "hello.bf"
 
-    readROM <- newROM (BS.unpack prog <> L.repeat 0)
-    (readRAM, writeRAM) <- newRAM
-
-    -- let dummyOut = CPUOut
-    --         { _progAddr = 0
-    --         , _memAddr = 0
-    --         , _memWrite = Nothing
-    --         , _output = Nothing
-    --         , _inputNeeded = False
-    --         }
-
-    flip evalStateT (initBFState, defaultOutput initBFState) $ forever $ do
-        (s, out@CPUOut{..}) <- get
-
-        instr <- liftIO $ readROM _progAddr
-        memRead <- liftIO $ readRAM _memAddr
-        let input = CPUIn
-                { outputAck = True
-                , input = Nothing
-                , ..
-                }
-        out'@CPUOut{..} <- zoom _1 $ mapStateT (return . runIdentity) $ step' input
-        zoom _2 $ put out'
-
-        for_ _output $ liftIO . putChar . chr . fromIntegral
-        for_ _memWrite $ liftIO . writeRAM _memAddr
-        return ()
+    runBFVec (loadVec (BS.unpack prog) 0) $
+      flip evalStateT (initBFState, defaultOutput initBFState) $
+        forever simulateCPU
