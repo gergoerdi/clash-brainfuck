585074bf5710c384f44d90f8fb6af390adbc9e17
Dr. ERDI Gergo <gergo@erdi.hu>
2020-04-10 14:40:33 +0800

    Factor out the "outside `world`" part of the simulator

--- src-clash/sim-logicboard.hs
+++ src-clash/sim-logicboard.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Clash.Prelude
 import Brainfuck
+import Brainfuck.Types (Cell)
 import Brainfuck.IO
 import Brainfuck.Memory (ProgSize, stringToROM)
 
@@ -13,6 +14,12 @@ import Data.Foldable (traverse_)
 import System.IO.Temp
 import Data.List as L
 
+world :: (MonadBFIO m) => (Bool, Maybe Cell) -> m (Maybe Cell, Bool)
+world (inputNeeded, output) = do
+    traverse_ doOutput output
+    input <- if inputNeeded then Just <$> doInput else return Nothing
+    return (input, True)
+
 main :: IO ()
 main = withSystemTempFile "brainfuck-.rom" $ \romFile romHandle -> do
     prog <- stringToROM <$> prepareIO
@@ -21,13 +28,11 @@ main = withSystemTempFile "brainfuck-.rom" $ \romFile romHandle -> do
 
     inChan <- newChan
     writeChan inChan (Nothing, False)
-    is <- getChanContents inChan
+    ins <- getChanContents inChan
+    let outs = simulateB @System (uncurry $ logicBoard romFile) ins
 
-    forM_ (simulateB @System (uncurry $ logicBoard romFile) is) $ \(inputNeeded, output) -> do
-        traverse_ doOutput output
-        input <- if inputNeeded then Just <$> doInput else return Nothing
-
-        writeChan inChan (input, True)
+    forM_ outs $ \out -> do
+        writeChan inChan =<< world out
 
 binLines :: Maybe Int -> [Word8] -> [String]
 binLines size bs = L.map (L.filter (/= '_') . show . pack) bytes
