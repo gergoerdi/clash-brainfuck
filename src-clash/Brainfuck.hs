11992bfc45bfde7b4ce5786c32b2831563e4d3cf
Dr. ERDI Gergo <gergo@erdi.hu>
2020-04-26 14:58:22 +0800

    Minor cleanup on `topEntity`

--- src-clash/Brainfuck.hs
+++ src-clash/Brainfuck.hs
@@ -15,20 +15,17 @@ topEntity
     :: "CLK"     ::: Clock System
     -> "BTN"     ::: Signal System (Active High)
     -> "ROWS"    ::: Signal System (Vec 4 (Active Low))
-    -> ( "SS"    ::: SevenSegment System 4 Low Low Low
-       , "COLS"  ::: Signal System (Vec 4 (Active Low))
+    -> ( "COLS"  ::: Signal System (Vec 4 (Active Low))
+       , "SS"    ::: SevenSegment System 4 Low Low Low
        )
 topEntity = withResetEnableGen board
   where
-    board btn rows =
-        ( display outbuf (enable inputNeededBuf inbuf)
-        , cols
-        )
+    board btn rows = (cols, display outbuf (enable inputNeededBuf inbuf))
       where
         (inputNeeded, output) = logicBoard "hello.rom" (enable ack inbuf) ack
         (cols, ack, inbuf) = inputs btn rows
 
-        outbuf = getFirst <$> integrate ack (First <$> output)
-        inputNeededBuf = getAny <$> integrate ack (Any <$> inputNeeded)
+        outbuf = fmap getFirst . integrate ack . fmap First $ output
+        inputNeededBuf = fmap getAny . integrate ack . fmap Any $ inputNeeded
 
 makeTopEntity 'topEntity
